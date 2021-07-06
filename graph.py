f066e8a6ee6d84487e421cf6a90c4c9e7cc400ff
TheGreatestShoaib <shoaibmadeinbangladesh@gmail.com>
2021-07-07 01:10:48 +0600

    Linear star Graph Tested

--- graph.py
+++ graph.py
@@ -1,25 +1,55 @@
 from turtle import *
 import time
+from mathseq import *
+
+seq = Sequences(0)
+#vanseq = seq.VanEck_seq()
+
+#vanseq = seq.fibonacci()
+
+#vanseq = seq.even_seq()
+
+#vanseq = seq.xibonacci(3)
 
 
-lists = [x*x for x in range(100,400)][::2]
+vanseq = seq.prime_generator(100000)
 
-radius = 120
+
+
+
+
+
+vans = []
+
+for _ in range(1000):
+	vans.append(next(vanseq))
+
+lists = [x*x for x in range(300,400)][::2]
+
+radius = 12
 
 turtle = Turtle()
-speed(3)
-
-for l in lists:
-	act = l // radius
-	print(act)
-	turtle.left(45)
-	time.sleep(0.5)
-	turtle.forward(act)
-	time.sleep(0.5)
-	turtle.left(45)
-	time.sleep(0.5)
-	turtle.backward(act)
-	time.sleep(0.5)
+turtle.speed(0)
 
 
+print(vans)
+def star(lists,rad):
+	for l in lists:
+		act = (lists[-1] / rad ) * l
+		print(act)
+		turtle.left(45)
+		#time.sleep(0.5)
+		turtle.forward(act)
+		#time.sleep(0.5)
+		turtle.left(45)
+		#time.sleep(0.5)
+		turtle.backward(act)
+		#time.sleep(0.5)
 
+
+
+
+
+star(vans,120)
+
+done()
