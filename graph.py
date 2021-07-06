15ec691cbd7590183128b08c4dd2be61d4858c5c
TheGreatestShoaib <shoaibmadeinbangladesh@gmail.com>
2021-07-07 01:56:36 +0600

    Added Linear Number Fall Graph

--- graph.py
+++ graph.py
@@ -29,27 +29,99 @@ lists = [x*x for x in range(300,400)][::2]
 radius = 12
 
 turtle = Turtle()
-turtle.speed(0)
+turtle.speed(1)
 
 
-print(vans)
+testfall = [100,200,230,0,2,4,5,0,1,1,0,0,1]
+
+
+
+#print(vans)
 def star(lists,rad):
 	for l in lists:
 		act = (lists[-1] / rad ) * l
 		print(act)
 		turtle.left(45)
-		#time.sleep(0.5)
 		turtle.forward(act)
-		#time.sleep(0.5)
-		turtle.left(45)
-		#time.sleep(0.5)
+		#turtle.left(45)
 		turtle.backward(act)
-		#time.sleep(0.5)
+
+
+
+
+def falldown(lists):
+	fall = 0
+	turtle.penup()
+	turtle.goto(-300,-0)
+	turtle.pendown()
+	for x in range(len(testfall)-1):
+		#print(test,"is at pos",x)
+		last = testfall[x]
+		hello = testfall[x+1]
+
+		if hello < last:
+			turtle.forward(100)
+			turtle.left(-85)
+			turtle.forward(100)
+			turtle.left(170)
+			turtle.forward(100)
+			turtle.left(-85)
+			turtle.forward(100)
+			#time.sleep(2)
+			turtle.backward(100)
+
+			fall += 1
+		else:
+			turtle.forward(100)
+			turtle.backward(100)
+			continue
+
+	print("falls :",fall)
 
 
 
 
 
-star(vans,120)
+
+
+
+
+#star(vans,120)
+falldown(testfall)
+
+
+"""
+
+turtle.forward(50)
+turtle.left(-90)
+turtle.forward(50)
+turtle.right(-180)
+turtle.left(180)
+#turtle.left(89)
+turtle.forward(50)
+time.sleep(1)
+turtle.backward(100)
+
+
+turtle.forward(100)
+turtle.left(-85)
+turtle.forward(100)
+turtle.left(170)
+turtle.forward(100)
+turtle.left(-85)
+turtle.forward(100)
+
+
+
+
+"""
+
+
+
+
+
+
+
 
 done()
+
